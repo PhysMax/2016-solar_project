3b9ff4e31f3081966d6d7139636bafdc00450016
Student <student@client2014.(none)>
2019-11-15 12:30:00 +0300

    Fix calculation of force, releaze movement

--- solar_model.py
+++ solar_model.py
@@ -19,9 +19,9 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        F = gravitational_constant*(body.m * space_objects.m)/(r**2)
-        body.Fx = F * abs((body.x - obj.x)) / r  # FIXME: нужно вывести формулу...
-        body.Fy = F * abs((body.y - obj.y)) / r  # FIXME: нужно вывести формулу...
+        F = gravitational_constant*(body.m * obj.m)/(r**2)
+        body.Fx += F * (obj.x - body.x) / r
+        body.Fy += F * (obj.y - body.y) / r
 
 
 def move_space_object(body, dt):
@@ -33,9 +33,12 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += body.Vx*dt
     body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+
+    ay = body.Fy/body.m
+    body.y += body.Vy*dt
+    body.Vy += ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
